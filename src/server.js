d7671ef5aa93338e50c497ad8fc4bfe71f453ff3
CodeCenturian <ashutoshkumariiitb@gmail.com>
2025-08-10 11:16:57 +0530

    removed turnoff

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import app from "./app.js";
 import dotenv from "dotenv";
+import fetch from "node-fetch";
 
 dotenv.config();
 
@@ -7,4 +8,11 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+
+
+  setInterval(() => {
+    fetch(`https://school-management-api-ndqu.onrender.com/`)
+      .then(() => console.log("Self-ping successful"))
+      .catch(err => console.error("Self-ping failed:", err.message));
+  }, 13 * 60 * 1000);
 });
